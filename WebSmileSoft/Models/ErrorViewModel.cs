4145a5c2b6b9889876eb6d30cbbcc16cd532b52a
Santiago Quintero Angarita <santiago.quintero@onoff.com.co>
2023-08-18 15:51:03 -0500

    Creación del proyecto

---
+++ WebSmileSoft/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WebSmileSoft.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
