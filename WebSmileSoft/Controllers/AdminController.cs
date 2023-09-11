3548567fdf96db7f724d6229d2cb19192851d2f2
JAVM93 <jhonvasquez9367@gmail.com>
2023-09-11 10:28:29 -0500

    CRUD Usuarios

--- WebSmileSoft/Controllers/AdminController.cs
+++ WebSmileSoft/Controllers/AdminController.cs
@@ -12,13 +12,55 @@ namespace WebSmileSoft.Controllers
         //private readonly RoleManager<IdentityRole> _roleManager;
 
 
-        public IActionResult Table()
-        {
-            return View("~/Views/Admin/ViewTable.cshtml");
-        }
+        //public IActionResult Table()
+        //{
+        //    return View("~/Views/Admin/ViewTable.cshtml");
+        //}
         public IActionResult Index()
         {
             return View("~/Views/Admin/Dashboard.cshtml");
+        }
+
+        //public IActionResult Usuarios()
+        //{
+        //    // Obtener datos de la base de datos (reemplaza esto con tu lógica)
+        //    //var usuarios = .ObtenerUsuarios();
+        //   // @usuario.Nombre 
+        //   //@usuario.Rol
+        //   //@usuario.Consultorio 
+        //   //@usuario.Edad 
+        //   //@usuario.UltimoIngreso 
+        //   //@usuario.Documento
+        //   //@usuario.TipoDocumento
+        //   //@usuario.FechaNacimiento
+        //   //@usuario.Genero
+        //   //@usuario.Direccion 
+        //   //@usuario.NumeroCelular
+        //   //@usuario.CorreoElectronico 
+        //    var usuarios = new[]
+        //    {
+        //        new { Id = 1, Nombre = "Juan", Rol="Administrador", Consultorio="Consultorio 1", Edad=25, UltimoIngreso="12/12/2020", Documento="123456789", TipoDocumento="Cedula", FechaNacimiento="12/12/1995", Genero="Masculino", Direccion="Calle 1", NumeroCelular="123456789", CorreoElectronico="smile@mile.com" },
+        //        new { Id = 2, Nombre = "Pedro", Rol="Usuario", Consultorio="Consultorio 2", Edad=25, UltimoIngreso="12/12/2020", Documento="123456789", TipoDocumento="Cedula", FechaNacimiento="12/12/1995", Genero="Masculino", Direccion="Calle 1", NumeroCelular="123456789", CorreoElectronico="user@mile.com"},
+        //        new { Id = 3, Nombre = "Maria", Rol="Usuario", Consultorio="Consultorio 3", Edad=25, UltimoIngreso="12/12/2020", Documento="123456789", TipoDocumento="Cedula", FechaNacimiento="12/12/1995", Genero="Masculino", Direccion="Calle 1", NumeroCelular="123456789", CorreoElectronico="mari@mile.com"}
+        //    };
+
+
+        //    // Pasa los datos a la vista
+        //    return View("~/Views/Admin/ViewTable.cshtml", usuarios);
+        //}
+
+        public IActionResult Table()
+        {
+            // Obtener datos de la base de datos (reemplaza esto con tu lógica)
+                var usuarios = new List<UsuariosModel>
+        {
+            new UsuariosModel { Id = 1, Nombre = "Juan", Rol="Administrador", Consultorio="Consultorio 1", Edad=25, UltimoIngreso=DateTime.Parse("12/12/2020"), Documento="123456789", TipoDocumento="Cedula", FechaNacimiento=DateTime.Parse("12/12/1995"), Genero="Masculino", Direccion="Calle 1", NumeroCelular="123456789", CorreoElectronico="smile@mile.com" },
+            new UsuariosModel { Id = 2, Nombre = "Pedro", Rol="Usuario", Consultorio="Consultorio 2", Edad=25, UltimoIngreso=DateTime.Parse("12/12/2020"), Documento="123456789", TipoDocumento="Cedula", FechaNacimiento=DateTime.Parse("12/12/1995"), Genero="Masculino", Direccion="Calle 1", NumeroCelular="123456789", CorreoElectronico="user@mile.com"},
+            new UsuariosModel { Id = 3, Nombre = "Maria", Rol="Usuario", Consultorio="Consultorio 3", Edad=25, UltimoIngreso=DateTime.Parse("12/12/2020"), Documento="123456789", TipoDocumento="Cedula", FechaNacimiento=DateTime.Parse("12/12/1995"), Genero="Masculino", Direccion="Calle 1", NumeroCelular="123456789", CorreoElectronico="mari@mile.com"}
+        };
+
+                // Pasa los datos a la vista
+                return View("~/Views/Admin/ViewTable.cshtml", usuarios);
         }
 
         //public AdminController(
