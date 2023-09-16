294a9fbd2df7b902631c2733bd8f91f870df42a3
JalejandroV93 <jhonvasquez93@utp.edu.co>
2023-09-16 13:33:22 -0500

    Actualizar Metodos

--- WebSmileSoft/Controllers/ProfileController.cs
+++ WebSmileSoft/Controllers/ProfileController.cs
@@ -1,16 +1,27 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 using NuGet.ContentModel;
 using WebSmileSoft.Models;
+using WebSmileSoft.Interfaces;
+using WebSmileSoft.Models.Response;
 
 namespace WebSmileSoft.Controllers
 {
     public class ProfileController : Controller
     {
+        private readonly ISettings _settings;
+        public ProfileController(ISettings settings)
+        {
+            _settings = settings;
+        }
+
         // GET: HomeController1
         public ActionResult Index()
         {
+            ViewBag.urlEndPoint = _settings.urlEndPoint;
             var especialidadesOdontologia = new List<EspecialidadOdontologia>
             {
                 new EspecialidadOdontologia { Id = 1, Nombre = "Odontología General", Descripcion = "Atención dental general" },
@@ -24,7 +35,39 @@ namespace WebSmileSoft.Controllers
             return View("~/Views/Profile/Profile.cshtml", especialidadesOdontologia);
         }
 
-       
-        
+
+        public ActionResult ChangePassword()
+        {
+            ViewBag.urlEndPoint = _settings.urlEndPoint;
+            return View("~/Views/Profile/_Security.cshtml");
+        }
+
+        [HttpPost]
+        public async Task<ChangePasswordViewModelResponse> ChangePassword([FromBody] ChangePasswordViewModelRequest Item)
+        {
+            var httpClient = new HttpClient();
+            //var content = new StringContent(JsonConvert.SerializeObject(ItemLogin), Encoding.UTF8, "application/json");
+
+            ChangePasswordViewModelResponse? changePasswordViewModelItem = new();
+            var response = await httpClient.PostAsJsonAsync(_settings.urlEndPoint + "/api/Users/v1/ChangePassword", Item);
+            if (response.IsSuccessStatusCode)
+            {
+                var json = await response.Content.ReadAsStringAsync();
+                JObject jsonObject = JObject.Parse(json);
+                var data = jsonObject["itemJson"];
+                string? jsonData = data != null ? data.ToString() : String.Empty;
+
+                if (!String.IsNullOrEmpty(jsonData))
+                {
+                    changePasswordViewModelItem = JsonConvert.DeserializeObject<ChangePasswordViewModelResponse>(jsonData);
+                }
+                return changePasswordViewModelItem!;
+            }
+            else
+                return changePasswordViewModelItem;
+        }
+
+
+
     }
 }
