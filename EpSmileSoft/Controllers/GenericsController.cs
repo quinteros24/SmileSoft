3ea698c08b1c202f65fb30a658545f1a358e5359
Santiago Torres <santiago.torres1@utp.edu.co>
2023-09-19 00:39:16 -0500

    CRUD_FRONT_USERS_3

---
+++ EpSmileSoft/Controllers/GenericsController.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Entities;
+using Domain.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EpSmileSoft.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    [Tags("Controlador encargado de gestionar los procesos genéricos")]
+    public class GenericsController : ControllerBase
+    {
+        private readonly IGenericsCore _genericsCore;
+        public GenericsController(IGenericsCore genericsCore)
+        {
+            _genericsCore = genericsCore;
+        }
+
+        [HttpGet]
+        [Route("v1/GenerateJWToken")]
+        public async Task<GenericResponseModel> GenerateJWToken([FromHeader] int uID, string userNameLogin, string token)
+        {
+            return await _genericsCore.GenerateJWToken(uID, userNameLogin, token);
+        }
+    }
+}
---
+++ WebSmileSoft/ViewComponents/NavbarViewComponent.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebSmileSoft.ViewComponents
+{
+    public class NavbarViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            return View(viewName: "_Navbar");
+        }
+    }
+}
