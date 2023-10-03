97e64ebccd93784a082b159da96ab1c64ec46e75
Santiago Torres <santiago.torres1@utp.edu.co>
2023-10-02 22:51:09 -0500

    Listar citas y Blobs

---
+++ Domain.Interfaces/Repository/IBlobsRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Domain.Entities;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repository
+{
+    public interface IBlobsRepository
+    {
+        // Agrega aquí cualquier método necesario para la interacción con el Blob Storage
+    }
+}
